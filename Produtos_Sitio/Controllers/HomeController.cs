1a660c9cfbb4ce7ec83a895bcc2c4831353f36f3
Rodrigo Rocha José <rodrigorochajose@hotmail.com>
2022-09-14 23:29:44 -0300

    Adicionar arquivos de projeto.

---
+++ Produtos_Sitio/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Produtos_Sitio.Models;
+using System.Diagnostics;
+
+namespace Produtos_Sitio.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Produtos_Sitio/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Produtos_Sitio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
